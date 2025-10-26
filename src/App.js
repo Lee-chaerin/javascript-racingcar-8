994bae8be1f2cc5e4da9faf48deb10bb4b911adb
Lee-chaerin <81899084+Lee-chaerin@users.noreply.github.com>
2025-10-27 04:39:35 +0900

    feat: 자동차 이름 입력받기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const CARS_INPUT = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const CARS = CARS_INPUT.split(',').map(car => car.trim());
+
+    Console.print(CARS);
+  }
 }
 
 export default App;

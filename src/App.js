b7c9494578a3526c9168cfe1a50fb7b1eda60285
Lee-chaerin <81899084+Lee-chaerin@users.noreply.github.com>
2025-10-27 04:42:13 +0900

    feat: 시도할 횟수 입력받기 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ class App {
   async run() {
     const CARS_INPUT = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const CARS = CARS_INPUT.split(',').map(car => car.trim());
+    const COUNT_INPUT = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
 
-    Console.print(CARS);
+    Console.print(CARS, COUNT_INPUT);
   }
 }
 

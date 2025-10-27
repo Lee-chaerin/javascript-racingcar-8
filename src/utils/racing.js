1229bf471463e51735eec230fc5b257a57aadcd2
Lee-chaerin <81899084+Lee-chaerin@users.noreply.github.com>
2025-10-27 13:34:57 +0900

    feat: 랜덤값으로 전진 여부 판별하기 기능 구현

--- src/utils/racing.js
+++ src/utils/racing.js
@@ -1,7 +1,12 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 export function racing(cars, count) {
   for (let round = 0; round < count; round++) {
-    Console.print(round);
+    Console.print(moveCheck());
   }
+}
+
+function moveCheck() {
+  const RANDOM_VALUE = Random.pickNumberInRange(0, 9);
+  return RANDOM_VALUE >= 4;
 }

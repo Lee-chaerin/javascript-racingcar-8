cc654ef0aaa9b4a14b9becc628ccf2bfe871685d
Lee-chaerin <81899084+Lee-chaerin@users.noreply.github.com>
2025-10-27 14:05:23 +0900

    feat: 차수별 실행 결과 출력하기 기능 구현

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App {
       const count = parseInt(countInput);
       validateCount(countInput, count);
 
+      Console.print("");
       racing(cars, count);
     } catch (error) {
       Console.print(error.message);
--- src/utils/racing.js
+++ src/utils/racing.js
@@ -2,16 +2,20 @@ import { Console, Random } from "@woowacourse/mission-utils";
 
 export function racing(cars, count) {
   const score = new Array(cars.length).fill(0);
+  Console.print("실행 결과");
 
   for (let round = 0; round < count; round++) {
     playRound(score, cars);
-    Console.print(score);
+    Console.print("");
   }
 }
 
 function playRound(score, cars) {
   for (let carIndex = 0; carIndex < cars.length; carIndex++) {
     updateScore(score, carIndex);
+
+    const progressBar = "-".repeat(score[carIndex]);
+    Console.print(`${cars[carIndex]} : ${progressBar}`);
   }
 }
 

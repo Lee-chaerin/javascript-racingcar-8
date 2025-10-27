86f07dd06863e11aef3b6016e2a6a40a8b4b4252
Lee-chaerin <81899084+Lee-chaerin@users.noreply.github.com>
2025-10-27 13:46:30 +0900

    feat: 자동차별 이동 거리 저장하기 기능 구현

--- src/utils/racing.js
+++ src/utils/racing.js
@@ -1,8 +1,23 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 export function racing(cars, count) {
+  const SCORE = new Array(cars.length).fill(0);
+
   for (let round = 0; round < count; round++) {
-    Console.print(moveCheck());
+    playRound(SCORE, cars);
+    Console.print(SCORE);
+  }
+}
+
+function playRound(score, cars) {
+  for (let carIndex = 0; carIndex < cars.length; carIndex++) {
+    updateScore(score, carIndex);
+  }
+}
+
+function updateScore(score, index) {
+  if (moveCheck()) {
+    score[index]++;
   }
 }
 
